208643d93e9d27f31fb805064da65c86065bfde0
Paweł Wiejkut <poczta@pawelwiejkut.net>
2021-04-03 13:49:59 +0200

    Update deploy-merged-build.sh

--- ci/deploy-merged-build.sh
+++ ci/deploy-merged-build.sh
@@ -6,7 +6,7 @@ wc -l ./zbw_trfn_tester.abap
 
 # Deploy artifacts
 git clone https://github.com/pawelwiejkut/bw_trfn_tester.git
-cp zbw_trfn_tester.abap ../bw_trfn_tester/zbw_trfn_tester.abap
+cp zbw_trfn_tester.abap /bw_trfn_tester/zbw_trfn_tester.abap
 cd ../bw_trfn_tester
 
 # Commit
